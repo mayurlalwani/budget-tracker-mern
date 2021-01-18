8b0071ec927637046bec835207d5b1b129a363b2
mayurlalwani <mayur_lalwani4@ymail.com>
2021-01-18 19:29:18 +0530

    Initialize project using CRA

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase";
+import firebaseAuth from "./firebase/firebaseConfig";
+import Dashboard from "./Components/Dashboard";
+import TransactionState from "./context/transactionState";
+
+function App() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [userId, setUserId] = useState("");
+  firebaseAuth.auth().onAuthStateChanged((user) => {
+    return user
+      ? setIsSignedIn(true) && setUserId(user.uid)
+      : setIsSignedIn(false);
+  });
+  const uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      // firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccess: () => false,
+    },
+  };
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      setIsSignedIn(!!user);
+      if (user.uid !== "") {
+        setUserId(user.uid);
+      }
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      {isSignedIn ? (
+        <span>
+          <div>Signed In!</div>
+          <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+          <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+          <img
+            alt="profile picture"
+            src={firebase.auth().currentUser.photoURL}
+          />
+          <TransactionState>
+            <Dashboard isSignedIn={isSignedIn} userId={userId} />
+          </TransactionState>
+        </span>
+      ) : (
+        <StyledFirebaseAuth
+          uiConfig={uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
