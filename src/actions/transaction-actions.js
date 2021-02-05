9cc35bd93d4457d476407c7d2f8aa3c00b389229
mayurlalwani <mayur_lalwani4@ymail.com>
2021-02-05 22:26:47 +0530

    Created actions and reducers to manage state

--- src/actions/transaction-actions.js
+++ src/actions/transaction-actions.js
@@ -1,11 +1,45 @@
 import * as types from "../constants/ActionTypes";
-import { getTransactionListApi } from "../services/transaction-service";
+import {
+  getTransactionListApi,
+  addTransactionApi,
+} from "../services/transaction-service";
 // export const addTransaction = () => ({ type: types.ADD_TRANSACTION, text });
-export const getTransactions = (userId) => ({
-  type: types.GET_TRANSACTIONS,
-  userId: userId,
-});
-export const clearTransactions = (id) => ({
-  type: types.CLEAR_TRANSACTIONS,
-  id,
-});
+
+const getTransactionsList = (data) => {
+  console.log({ data });
+  return {
+    type: types.GET_TRANSACTIONS,
+    payload: data,
+  };
+};
+
+const setTransactionsList = (data) => {
+  return {
+    type: types.ADD_TRANSACTION,
+    payload: data,
+  };
+};
+
+export const getTransactionsAction = (data) => {
+  return async (dispatch) => {
+    try {
+      const response = await getTransactionListApi(data);
+      console.log({ response });
+      if (response.status === 200) {
+        dispatch(getTransactionsList(response.data));
+      }
+    } catch (error) {}
+  };
+};
+
+export const addTransactionAction = (data) => {
+  return async (dispatch) => {
+    try {
+      const response = await addTransactionApi(data);
+      console.log({ response });
+      if (response.status === 200) {
+        dispatch(setTransactionsList(response.data));
+      }
+    } catch (error) {}
+  };
+};
--- src/services/transaction-service.js
+++ src/services/transaction-service.js
@@ -1,5 +1,18 @@
 import axios from "axios";
 import * as types from "../constants/ActionTypes";
+import firebaseAuth from "../firebase/firebaseConfig";
+
+const createToken = async () => {
+  const user = firebaseAuth.auth().currentUser;
+  const token = user && (await user.getIdToken());
+  const payloadHeader = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return payloadHeader;
+};
 
 export const getTransactionListApi = async (userId) => {
   const header = await createToken();
@@ -9,8 +22,24 @@ export const getTransactionListApi = async (userId) => {
       userId,
       header
     );
-    dispatch({ type: types.GET_TRANSACTIONS, payload: res.data });
-  } catch (err) {
-    dispatch({ type: types.TRANSACTION_ERROR, payload: err.response });
-  }
+    return res;
+  } catch (err) {}
+};
+
+export const addTransactionApi = async (transaction) => {
+  const header = await createToken();
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  try {
+    const res = await axios.post(
+      "http://localhost:4000/api/transactions/addTransactionDetails",
+      transaction,
+      // config,
+      header
+    );
+    return res;
+  } catch (err) {}
 };
